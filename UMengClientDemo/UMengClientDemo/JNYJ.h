f230e5eefd1ccbbe9a237619351a1dc882c9e92c
JNYJ(William.zhou) <qqiang.dev@gmail.com>
2014-11-28 14:26:08 +0800

    project create

---
+++ UMengClientDemo/UMengClientDemo/JNYJ.h
@@ -0,0 +1,13 @@
+//
+//  JNYJ.h
+//  UMengClientDemo
+//
+//  Created by JNYJ on 14-11-28.
+//  Copyright (c) 2014年 JNYJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JNYJ : NSObject
+
+@end
---
+++ UMengClientDemo/UMengClientDemo/JNYJ.m
@@ -0,0 +1,13 @@
+//
+//  JNYJ.m
+//  UMengClientDemo
+//
+//  Created by JNYJ on 14-11-28.
+//  Copyright (c) 2014年 JNYJ. All rights reserved.
+//
+
+#import "JNYJ.h"
+
+@implementation JNYJ
+
+@end
